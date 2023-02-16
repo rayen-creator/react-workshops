823f43f543eedd2641d8c3688bdfe7b8d785c559
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-16 12:22:01 +0100

    to do atelier2

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,31 +1,29 @@
 import Product from "./Product";
-import listproducts from "../products.json"
-import React, {Component} from 'react';
+import listproducts from "../products.json";
+import React, { Component } from "react";
 import { Row } from "react-bootstrap";
 
-export default class Products extends Component{
-    render() {
-return (
-    <div>
-    <Row className="d-flex justify-content-center p-4">
-      {
-        listproducts.map((product,index)=>(
-        <Product 
-        key={index}
-        // product={product}
-        name={product.name} 
-        price = {product.price}
-        img= {product.img}
-        description= {product.description}
-        like={product.like}
-        quantity={product.quantity}
-        />
-    ))}
-    </Row>
-  
-    </div>
-
-   
-)
-    }
+export default class Products extends Component {
+  render() {
+    return (
+      <div>
+       
+        <Row className="d-flex justify-content-center p-4">
+          {listproducts.map((product, index) => (
+            <Product
+              key={index}
+              // product={product}
+              name={product.name}
+              price={product.price}
+              img={product.img}
+              description={product.description}
+              like={product.like}
+              quantity={product.quantity}
+            />
+          ))}
+        </Row>
+      
+      </div>
+    );
+  }
 }
