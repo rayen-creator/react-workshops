d116019ad5b595dfce7c08553883e3b8b38ef7b9
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-18 22:21:25 +0100

    ⚡️ update product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
+import "./product.css";
 
 export function Product(props) {
   const [like, setLike] = useState(props.like);
@@ -11,7 +12,8 @@ export function Product(props) {
   };
 
   return (
-    <Card style={{ width: "17rem" }} className="m-2">
+   like >=5 ? (
+   <Card style={{ width: "17rem" }} className="bestProduct m-2">
       <Card.Img
         variant="top"
         src={require("../assets/images/" + props.img)}
@@ -42,5 +44,38 @@ export function Product(props) {
         </Button>
       </Card.Body>
     </Card>
+    ):(
+      <Card style={{ width: "17rem" }} className=" m-2">
+      <Card.Img
+        variant="top"
+        src={require("../assets/images/" + props.img)}
+        width="100%"
+        height="200px"
+      />
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>Description : {props.description}</Card.Text>
+        <Card.Text>Price : {props.price} DT</Card.Text>
+        <Card.Text>Like : {like}</Card.Text>
+        <Card.Text>quantity : {props.quantity}</Card.Text>
+
+        <Button
+          variant="primary"
+          style={{ marginRight: "50px" }}
+          onClick={()=>likeProduct()}
+        >
+          Like
+        </Button>
+        <Button
+          variant="info"
+          style={{ marginLeft: "50px" }}
+          onClick={props.onClick}
+          disabled={props.quantity === 0}
+        >
+          Buy
+        </Button>
+      </Card.Body>
+    </Card>
+    )
   );
 }
