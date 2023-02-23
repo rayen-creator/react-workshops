b35a5e6159e87757a5187b5ae510288472a66ba6
Rayen Oueslati <rayenoueslati60@gmail.com>
2023-02-23 12:06:51 +0100

    ✅ update routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import {React } from "react";
-import { Products } from './components/Products';
+import "./App.css";
+import { React } from "react";
+import { Products } from "./components/Products";
+import { Route, Routes } from "react-router-dom";
+import { NotFound } from "./components/NotFound";
+import { NavbarComponent } from "./components/Navbar";
 
 function App() {
   return (
     <div>
-    <Products />
+      <NavbarComponent />
+      <Routes>
+        <Route path="/">
+          <Route  exact path="/products" element={<Products />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
